8a287673c770054ca176c0411eb216c724899c15
Yakuzokai <albertvelez025@gmail.com>
2026-09-23 23:11:41 +0800

    feat: add postgres init script for all service databases

---
+++ infrastructure/docker/init-scripts/init-databases.sql
@@ -0,0 +1,12 @@
+-- Initialize all microservice databases
+CREATE DATABASE chat_db;
+CREATE DATABASE user_db;
+CREATE DATABASE review_db;
+CREATE DATABASE payment_db;
+CREATE DATABASE fraud_detection;
+CREATE DATABASE ml_service;
+CREATE DATABASE analytics;
+CREATE DATABASE auth_db;
+CREATE DATABASE product_db;
+CREATE DATABASE order_db;
+CREATE DATABASE notification_db;
